16e843d74cdd428ed7193d3e5a8e5bbe904835b0
Mohammad Tafreshi <mn.tafreshi@gmail.com>
2024-01-21 10:38:07 +0330

    ss

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Tehran"));
+		System.out.println(now);
 	}
 
 }
